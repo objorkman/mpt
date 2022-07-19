626ca3ffe1e4e0d93561b697c05e9b4f69e82def
Oscar Bjorkman <oscarb@berkeley.edu>
2022-07-19 22:52:33 +0000

    Update script

--- demo/time_script.py
+++ demo/time_script.py
@@ -1,4 +1,5 @@
 import os
+import csv
 from timeit import default_timer as timer
 import math
 
@@ -6,18 +7,28 @@ def percentile(data, perc):
     size = len(data)
     return sorted(data)[int(math.ceil((size * perc) / 100)) - 1]
 
-diffs = []
-threads = 32
-for i in range(50):
-    start = timer()
-    os.system('OMP_NUM_THREADS=' + str(threads) + ' build/Release/nao_cup_planning-KDTreeBatch-double-mt -S')
-    end = timer()
-    diffs.append(end - start) 
+result= []
+data = []
+threads = [1, 2, 4, 8, 16, 32, 48]
 
-print('Mean: ', sum(diffs) / len(diffs))
-print('25%: ', percentile(diffs, 25))
-print('50%: ', percentile(diffs, 50))
-print('75%: ', percentile(diffs, 75))
-print('90%: ', percentile(diffs, 90))
-print('95%: ', percentile(diffs, 95))
-print('99%: ', percentile(diffs, 99))
+for t in threads:
+    times = []
+    for i in range(3):
+        start = timer()
+        os.system('OMP_NUM_THREADS=' + str(t) + ' build/Release/nao_cup_planning-KDTreeBatch-double-mt -S')
+        end = timer()
+        times.append(end - start) 
+    
+    percents = [25, 50, 75, 95, 99]
+    result.append([sum(times) / len(times)] + [percentile(times, p) for p in percents])
+    
+    data.append(times)
+
+with open('result.csv', 'w', newline='') as file:
+    writer = csv.writer(file, delimiter=',')
+    writer.writerow(['mean'] + [str(t) for t in threads])
+    writer.writerows(result)
+
+with open('data.csv', 'w', newline='') as file:
+    writer = csv.writer(file, delimiter=',')
+    writer.writerows(data)
