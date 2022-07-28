797aa99472c55056bdc41bf827dd355509b2560b
Oscar Bjorkman <oscarb@berkeley.edu>
2022-07-28 13:05:52 -0700

    Update for multi-node

--- demo/time_script.py
+++ demo/time_script.py
@@ -2,6 +2,7 @@ import os
 import csv
 from timeit import default_timer as timer
 import math
+from docker import Client
 
 def percentile(data, perc):
     size = len(data)
@@ -12,23 +13,31 @@ data = []
 threads = [1, 2, 4, 8, 16, 32, 48]
 percents = [25, 50, 75, 95, 99]
 
+cli = Client(base_url='unix://var/run/docker.sock')
+HOSTNAME = os.environ.get("HOSTNAME")
+all_containers = cli.containers()
+our_container = [c for c in all_containers if c['Id'][:12] == HOSTNAME[:12]][0]
+
+container_num = our_container['Labels']['com.docker.compose.container-number']
+
 for t in threads:
     times = []
-    for i in range(50):
+    for i in range(2):
         start = timer()
         os.system('OMP_NUM_THREADS=' + str(t) + ' build/Release/se3_rigid_body_planning-GNAT-double-mt -S /omplapp/resources/3D/Twistycool.cfg')
         end = timer()
-        times.append(end - start) 
-    
+        times.append(end - start)
+
     result.append([sum(times) / len(times)] + [percentile(times, p) for p in percents])
-    
+
     data.append(times)
 
-with open('result.csv', 'w', newline='') as file:
+with open('../../data/result_' + container_num + '.csv', 'w', newline='') as file:
     writer = csv.writer(file, delimiter=',')
     writer.writerow(['mean'] + [str(p) for p in percents])
     writer.writerows(result)
 
-with open('data.csv', 'w', newline='') as file:
+with open('../../data/data_' + container_num + '.csv', 'w', newline='') as file:
     writer = csv.writer(file, delimiter=',')
     writer.writerows(data)
+
